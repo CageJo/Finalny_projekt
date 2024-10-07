00fb10c34f97f40ab6cafe34f7e690c1d756aeeb
Ján Kisty <kisty.jan@gmail.com>
2024-10-07 20:38:18 +0200

    final version 1.1

--- Evidencia_poistenia/main.py
+++ Evidencia_poistenia/main.py
@@ -1,9 +1,11 @@
-from main_screen import main_screen
+from main_screen import UserInterface
+
+try:
+    # spustenie_programu - metoda
+    program = UserInterface()
+    program.main_screen()
+    print()
 
 #vypinanie uzivatelom a test vstupu klavesy
-try:
-    # spustenie_program
-    main_screen()
-    print()
 except KeyboardInterrupt:
     print("\nProgram bol prerušený používateľom alebo prostredníctvom CTRL+C.")
--- Evidencia_poistenia/main_screen.py
+++ Evidencia_poistenia/main_screen.py
@@ -1,104 +1,107 @@
-from class_Poistena_Osoba import PoistenaOsoba
+from poistena_osoba import PojistenaOsoba
 
-list_pojistenych = []
 
-def pridaj_zaznam(zaznamy, novy_zaznam):
-    if novy_zaznam in zaznamy:
-        print("Upozornenie: Tento záznam už existuje!")
-    else:
-        zaznamy.append(novy_zaznam)
-        print("Záznam bol úspešne pridaný.")
+class UserInterface:
 
-#class UserInterface:
-def main_screen():
-    while True:
-        print("------------------------------")
-        print("Evidence pojistenych")
-        print("------------------------------")
-        print("Vyberte si akci:")
-        print("1. Pridat nového pojisteného")
-        print("2. Vypsat vsechny pojistené")
-        print("3. Vyhledat pojisteného")
-        print("4. Konec")
+    def __init__(self):
+        self.list_pojistenych = []
 
-        try:
-            vyber = input("Zadaj číslo: ")
-            vyber = int(vyber)
-        except ValueError:
-            print("Neplatný výběr, zkuste to znovu.")
-            continue
-        """presunute do main.py
-        except KeyboardInterrupt:
-            print("\nProgram bol prerušený používateľom alebo prostredníctvom CTRL+C.")
-            break"""
+    def pridaj_zaznam(self, zaznamy, novy_zaznam):
+        if novy_zaznam in zaznamy:
+            print("Upozornění: Tento záznam již existuje!")
+        else:
+            zaznamy.append(novy_zaznam)
+            print("Záznam byl úspěšně přidán.")
 
-        match vyber:
-            case 1:
-                while True:
-                    print("Zadajte jméno pojištěného")
-                    jmeno = input().replace(" ", "")
-                    if not jmeno:
-                        print("Jméno nesmí být prázdné.")
-                        continue
-                    break
+    def main_screen(self):
+        while True:
+            print("------------------------------")
+            print("Evidence pojištěných")
+            print("------------------------------")
+            print("Vyberte si akci:")
+            print("1. Přidat nového pojištěného")
+            print("2. Vypsat všechny pojištěné")
+            print("3. Vyhledat pojištěného")
+            print("4. Konec")
 
-                while True:
-                    print("Zadajte príjmení")
-                    prijmeni = input().replace(" ", "")
-                    if not prijmeni:
-                        print("Příjmení nesmí být prázdné.")
-                        continue
-                    break
+            try:
+                vyber = input("Zadej číslo: ")
+                vyber = int(vyber)
+            except ValueError:
+                print("Neplatný výběr, zkuste to znovu.")
+                continue
+            """opravuje error, ktory vyhadzuje pri force ukonceni, presunute do main.py
+            except KeyboardInterrupt:
+                print("\nProgram bol prerušený používateľom alebo prostredníctvom CTRL+C.")
+                break"""
 
-                while True:
-                    print("Zadajte telefonní číslo:")
-                    cislo = input().strip().replace(" ", "")
-
-                    if not cislo.isdigit() or len(cislo) < 6:
-                        print("Zadajte platné telefonní číslo.")
-                        continue
-                    else:
-                        cislo_upravene = int(cislo)
+            match vyber:
+                case 1:
+                    while True:
+                        print("Zadejte jméno pojištěného")
+                        jmeno = input().replace(" ", "")
+                        if not jmeno:
+                            print("Jméno nesmí být prázdné.")
+                            continue
                         break
 
-                while True:
-                    print("Zadajte vek")
-                    vek = input().replace(" ", "")
-                    if not vek.isdigit() or int(vek) > 150:
-                        print("Zadaj správny vek, v rozpätí <0 - 150>.")
-                        continue
-                    else:
-                        vek = int(vek)
+                    while True:
+                        print("Zadejte příjmení")
+                        prijmeni = input().replace(" ", "")
+                        if not prijmeni:
+                            print("Příjmení nesmí být prázdné.")
+                            continue
                         break
 
-                nova_osoba = PoistenaOsoba(jmeno=jmeno, prijmeni=prijmeni, cislo=cislo, vek=vek)
-                pridaj_zaznam(list_pojistenych, nova_osoba)
+                    while True:
+                        print("Zadajte telefonní číslo:")
+                        cislo = input().strip().replace(" ", "")
 
-                print("Data byla ulozena. Pokracujte klavesou ENTER...")
-                input()
+                        if not cislo.isdigit() or len(cislo) < 6:
+                            print("Zadajte platné telefonní číslo.")
+                            continue
+                        else:
+                            cislo_upravene = int(cislo)
+                            break
 
-            case 2:
-                print()
-                for osoba in list_pojistenych:
-                    print(osoba)
-                input("Pokračujte klávesou ENTER")
+                    while True:
+                        print("Zadejte věk")
+                        vek = input().replace(" ", "")
+                        if not vek.isdigit() or int(vek) > 150:
+                            print("Zadejte správný věk, v rozmezí <0 - 150>")
+                            continue
+                        else:
+                            vek = int(vek)
+                            break
 
-            case 3:
-                print("Vyhledat pojisteného")
-                hledane_jmeno = input("Zadajte jméno pojisteného: ")
-                hledane_prijmeni = input("Zadajte prijmeni: ")
-                nalezeno = False
-                for osoba in list_pojistenych:
-                    if osoba.jmeno == hledane_jmeno and osoba.prijmeni == hledane_prijmeni:
+                    nova_osoba = PojistenaOsoba(jmeno=jmeno, prijmeni=prijmeni, cislo=cislo, vek=vek)
+                    self.pridaj_zaznam(self.list_pojistenych, nova_osoba)
+
+                    print("Data byla uložena. Pokračujte klávesou ENTER…")
+                    input()
+
+                case 2:
+                    print()
+                    for osoba in self.list_pojistenych:
                         print(osoba)
-                        nalezeno = True
-                if not nalezeno:
-                    print("Pojistený nenalezen.")
+                    input("Pokračujte klávesou ENTER")
+
+                case 3:
+                    print("Vyhledat pojištěného")
+                    hledane_jmeno = input("Zadejte jméno pojištěného: ")
+                    hledane_prijmeni = input("Zadejte příjmení pojištěného: ")
+                    nalezeno = False
+                    for osoba in self.list_pojistenych:
+                        if osoba.jmeno == hledane_jmeno and osoba.prijmeni == hledane_prijmeni:
+                            print(osoba)
+                            nalezeno = True
+                    if not nalezeno:
+                        print("Pojištěný nenalezen.")
 
 
-                input("Pokračujte klávesou ENTER")
+                    input("Pokračujte klávesou ENTER")
 
-            case 4:
-                print("Konec")
-                break
+                case 4:
+                    print("Konec")
+                    break
 
